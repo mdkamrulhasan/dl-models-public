62663ea94c2263698843a97b49181e7100a2cc48
Kamrul Hasan <hasanka@gvsu.edu>
2025-11-10 12:58:00 -0500

    cnn24x24 added

---
+++ cnns/cnn24x24_pytorch.py
@@ -0,0 +1,177 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+# from models import MyVanillaCNNNet as Net
+from models import Net24x24 as Net
+
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import os
+import pickle
+import pandas as pd
+
+
+# 1. Define a custom Data Loader
+class NumpyDataset(Dataset):
+    def __init__(self, X, Y=None, transform=None):
+        self.X = torch.from_numpy(X).float()  # convert to float tensor
+        if Y is not None:
+            self.Y = torch.from_numpy(Y).long()  # convert to long tensor
+        else:
+            self.Y = torch.from_numpy(np.zeros(len(self.X))).long()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        x = self.X[idx]
+        y = self.Y[idx]
+
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+
+def accuracy(model, data_loader):
+    """Accuracy estimation."""
+
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+
+    output_labels = []
+
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data
+            # cpu vs gpu
+            if device != 'cpu':
+                images, labels = images.to(device), labels.to(device)
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs, 1)
+            output_labels += predicted.tolist()
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return 100 * correct // total, output_labels
+
+
+def evaluate(estimate_training_accuracy=False):
+    """model evaluation on the test set."""
+
+    # Load previously trained model
+    net = Net()
+    net.to(device)
+    net.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
+
+    if estimate_training_accuracy:
+        # Evaluate on training data
+        train_accuracy, _ = accuracy(net, trainloader)
+        print(f'Accuracy of the network on training images: {train_accuracy} %')
+
+    # Evaluate on test data
+    test_accuracy, test_output_labels = accuracy(net, testloader)
+    # print(f'Accuracy of the network on the 10000 test images: {test_accuracy} %')
+
+    return test_output_labels
+
+
+def training(n_epoch=2, learning_rate=0.001, momentum=0.9):
+    """Training a CNN model."""
+
+    # CNN model instantiation
+    net = Net()
+    net.to(device)
+    # Loss function
+    criterion = nn.CrossEntropyLoss()
+    # Optimizer
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+    # Model/network training
+    for epoch in range(n_epoch):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            # cpu vs gpu
+            if device != 'cpu':
+                inputs, labels = inputs.to(device), labels.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            # print statistics (every 2000 mini-batches)
+            if i % 2000 == 1999:
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+    # Save the model for future reference
+    torch.save(net.state_dict(), MODEL_PATH)
+    print('Finished Training')
+
+
+RUN_LOCALLY = True
+BATCH_SIZE = 4
+
+if RUN_LOCALLY:
+    # Your local machine paths
+    MODEL_PATH = '/Users/hasanka/projects/dl-models-public/models/24x24_challange_net.pth'
+    DATA_PATH = '/Users/hasanka/projects/dl-models-public/data/24x24-challange'
+else:
+    # Your server paths
+    MODEL_PATH = '/mnt/home/hasanka/projects/dl-models-public/models/24x24_challange_net.pth'
+    DATA_PATH = '/mnt/home/hasanka/projects/dl-models-public/data/24x24-challange'
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+# base_path = '/content/drive/MyDrive/data/fall-2025-ass3'
+
+# Training data
+with open(os.path.join(DATA_PATH, 'train_X_y.pkl'), 'rb') as f:
+    X_train, y_train = pickle.load(f)
+
+# Test data
+with open(os.path.join(DATA_PATH, 'test_X.pkl'), 'rb') as f:
+    X_test = pickle.load(f)
+
+# Reshaping data matrices
+X_train = np.transpose(X_train.astype(int), (0, 3, 1, 2))
+X_test = np.transpose(X_test.astype(int), (0, 3, 1, 2))
+
+# Load and normalize  data
+transform = transforms.Compose(
+    [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# batch_size = 4
+
+train_dataset = NumpyDataset(X_train, y_train.flatten(), transform=transform)
+trainloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+test_dataset = NumpyDataset(X_test, transform=transform)
+testloader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE,
+                                         shuffle=False, num_workers=2)
+
+if __name__ == '__main__':
+    # model training
+    training()
+    # model evaluation on the test set
+    test_output_labels = evaluate(estimate_training_accuracy=True)
+
+    # Pack results in a pandas dataframe
+
+    test_predictions = pd.DataFrame({
+        'rowId': range(0, len(test_output_labels)),
+        'label': test_output_labels})
+
+    test_predictions.to_csv(os.path.join(DATA_PATH, 'my_cnn_results.csv'), index=False)
--- cnns/models.py
+++ cnns/models.py
@@ -55,3 +55,26 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+class Net24x24(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 3 * 3, 120)  # updated input size
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 100)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# # CNN model instantiation
+# net = Net24x24()
